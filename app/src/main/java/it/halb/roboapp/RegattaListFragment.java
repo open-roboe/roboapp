2e02244e286661ee2c7da98d05b57a74edcf4722
robalb <11277482+robalb@users.noreply.github.com>
2023-02-27 23:54:32 +0100

    Added viewmodel to regattalist, ready to work on recyclerview

--- app/src/main/java/it/halb/roboapp/RegattaListFragment.java
+++ app/src/main/java/it/halb/roboapp/RegattaListFragment.java
@@ -2,17 +2,31 @@ package it.halb.roboapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import it.halb.roboapp.databinding.FragmentRegattaListBinding;
+
 public class RegattaListFragment extends Fragment {
+    private FragmentRegattaListBinding binding;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_regatta_list, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentRegattaListBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        RegattaListViewModel model = new ViewModelProvider(this).get(RegattaListViewModel.class);
+        binding.setLifecycleOwner(this.getViewLifecycleOwner());
+        binding.setRegattaListViewModel(model);
     }
 }
---
+++ app/src/main/java/it/halb/roboapp/RegattaListViewModel.java
@@ -0,0 +1,35 @@
+package it.halb.roboapp;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+import java.util.Random;
+
+import it.halb.roboapp.data.Repository;
+import it.halb.roboapp.data.local.Regatta;
+
+public class RegattaListViewModel extends AndroidViewModel {
+
+    private final LiveData<List<Regatta>> allRegattas;
+    private final Repository repository;
+
+    public RegattaListViewModel(@NonNull Application application) {
+        super(application);
+        repository = new Repository(application);
+        allRegattas = repository.getAllRegattas();
+    }
+
+    public String getTest(){
+        return "tesst";
+    }
+
+    public void fakeInsert(){
+        Random rand = new Random();
+        String name = "name" + String.valueOf(rand.nextInt());
+        repository.insertRegatta(new Regatta(name, "type", 123));
+    }
+}
