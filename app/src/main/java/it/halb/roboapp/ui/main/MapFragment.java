c89fa5f812b95a0958b92400f49a0cab6cdf6e3b
isiuni <i.delrosso@campus.unimib.it>
2023-03-14 12:00:48 +0100

    add googleMap

--- app/src/main/java/it/halb/roboapp/ui/main/MapFragment.java
+++ app/src/main/java/it/halb/roboapp/ui/main/MapFragment.java
@@ -1,9 +1,13 @@
 package it.halb.roboapp.ui.main;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -11,15 +15,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import it.halb.roboapp.R;
 import it.halb.roboapp.databinding.FragmentLoginBinding;
 import it.halb.roboapp.databinding.FragmentMapBinding;
 import it.halb.roboapp.ui.main.MapViewModel;
 
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment{
 
     private FragmentMapBinding binding;
+    private SupportMapFragment supportmapfragment;
+    Context c;
 
     public MapFragment() {
 
@@ -30,6 +40,30 @@ public class MapFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         binding = FragmentMapBinding.inflate(inflater, container, false);
+        c = this.getContext();
+
+        supportmapfragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
+
+        supportmapfragment.getMapAsync(googleMap -> {
+
+            if (ActivityCompat.checkSelfPermission(c, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(c, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                            PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            googleMap.setMyLocationEnabled(true);
+
+
+        });
+
         return binding.getRoot();
     }
 
@@ -41,4 +75,6 @@ public class MapFragment extends Fragment {
         binding.setLifecycleOwner(this.getViewLifecycleOwner());
         binding.setMapViewModel(model);
     }
+
+
 }
